a6fc5620a63b65950b7445e9d6f0a799399eb61f
Taimoor <taimoor.hassan3001@gmail.com>
2022-04-14 01:06:11 +0500

    metaMask connected with react App

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WalletCard from "./WalletCard";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WalletCard />
   </React.StrictMode>,
   document.getElementById('root')
 );
